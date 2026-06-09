ccaef9da6ab3771bffa670307ed0c3f764c2c26f
Tejas Chapegadikar <tejaschapegadikar15@gmail.com>
2026-06-09 23:44:02 +0530

    finished with (multiple inheritance in Python)

--- multiple _inheritance.py
+++ multiple _inheritance.py
@@ -1,1 +1,29 @@
 # multiple inheritance is a concept when a child class is derived from more than one parent class.
+
+class prey:
+
+    def flee(self):
+        print("This animal flee's")
+
+class predator:
+
+    def hunt(self):
+        print("This animal hunt's")
+    
+
+class Rabbit(prey):
+    pass
+class Hawk(predator):
+    pass
+class Fish(prey, predator):
+    pass
+
+rabbit = Rabbit()
+hawk = Hawk()
+fish = Fish()
+
+rabbit.flee()
+hawk.hunt()
+
+fish.flee()
+fish.hunt()
