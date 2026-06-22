70385470653c63c37d80412de06b47d70a03fe62
Tejas Chapegadikar <tejaschapegadikar15@gmail.com>
2026-06-23 02:17:41 +0530

    completed (function to variable) in Python

---
+++ functions_to_variable.py
@@ -0,0 +1,12 @@
+# this is bcoz, we are assigning the memory adderss of "hello" to the variable "hi".
+
+def hello():
+    print("Hello")
+
+hi = hello
+hi()
+
+# also sopme different things :
+
+greetings = print
+greetings("Hii!! How are you?")
