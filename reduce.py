2178461d10f562a935efeb4081c70db6929d3098
Tejas Chapegadikar <tejaschapegadikar15@gmail.com>
2026-07-01 22:33:42 +0530

    completed (reduce functions) in python

--- reduce.py
+++ reduce.py
@@ -3,3 +3,13 @@
 
 # reduce(function, iterable
 # a list is a type of iterable.
+
+import functools
+
+#letters = ["h", "e", "l", "l", "o"]
+#word = functools.reduce(lambda x, y : x+y, letters)
+#print(word)
+
+factorial = [ 5, 4, 3, 2, 1,]
+result = functools.reduce(lambda x, y : x * y, factorial)
+print(result)
