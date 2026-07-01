d45e9a63afce6642f3eb87793ef2882e9440eae7
Tejas Chapegadikar <tejaschapegadikar15@gmail.com>
2026-07-01 23:12:04 +0530

    started with (list_comprehension) in python

---
+++ list_comprehension.py
@@ -0,0 +1,5 @@
+# List comprehention : a way to create a new list with less syntax
+# can mimic certain lsmbds functions, easier to read
+# list = [expression for item in iterable]
+
+square = []
--- reduce.py
+++ reduce.py
@@ -10,6 +10,7 @@ import functools
 #word = functools.reduce(lambda x, y : x+y, letters)
 #print(word)
 
-factorial = [ 5, 4, 3, 2, 1,]
+factorial = [5, 4, 3, 2, 1]
 result = functools.reduce(lambda x, y : x * y, factorial)
 print(result)
+
