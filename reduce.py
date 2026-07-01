33bc9d8229a4a1c0396679e6138bbffc0a827766
Tejas Chapegadikar <tejaschapegadikar15@gmail.com>
2026-07-01 20:00:36 +0530

    started with (reduce_functions) in python

---
+++ reduce.py
@@ -0,0 +1,5 @@
+# reduce() = apply a function to an iterable and reduce it to a single cumulative value.
+# performs functions on first two elements and repeats process until 1 value remains
+
+# reduce(function, iterable
+# a list is a type of iterable.
